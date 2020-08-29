69cf33d6decb05bbc2c61e2beec28b1c09fd7ef8
Rahul Sen <rahulsen1992@hotmail.com>
2020-08-29 18:38:55 +0530

    Added splash screen

---
+++ GoToto_Rider/Activities/SpalashActivity.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Android.App;
+using Android.OS;
+
+namespace GoToto_Rider.Activities
+{
+    [Activity(Label = "@String/app_name", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait )]
+    public class SpalashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Thread.Sleep(2000);
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
