f81402150736a0a61ed230f01a5219436e609cad
Rahul Sen <rahulsen1992@hotmail.com>
2020-09-05 14:12:43 +0530

    Added functionbality to move the point to the current location

--- GoToto_Rider/MainActivity.cs
+++ GoToto_Rider/MainActivity.cs
@@ -9,6 +9,11 @@ using Firebase;
 using Android.Views;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
+using Android;
+using Android.Support.V4.App;
+using Android.Content.PM;
+using Android.Gms.Location;
+using GoToto_Rider.Helpers;
 
 namespace GoToto_Rider
 {
@@ -20,6 +25,18 @@ namespace GoToto_Rider
         Android.Support.V4.Widget.DrawerLayout drawerLayout;
 
         GoogleMap mainMap;
+
+        readonly string[] permissionGroupLocation = { Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessCoarseLocation };
+        const int requestLocationId = 0;
+
+        LocationRequest mLocationRequest;
+        FusedLocationProviderClient locationClient;
+        Android.Locations.Location mLastLocation;
+        LocationCallbackHelper mLocationCallback;
+
+        static int UPDATE_INTERVAL = 5; //5 SECONDS
+        static int FASTEST_INTERVAL = 5;
+        static int DISPLACEMENT = 3; //meters
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -30,6 +47,10 @@ namespace GoToto_Rider
 
             SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
             mapFragment.GetMapAsync(this);
+            CheckLocationPermission();
+            CreateLocationRequest();
+            GetMyLocation();
+            StartLocationUpdates();
         }
 
         void ConnectControl()
@@ -92,12 +113,80 @@ namespace GoToto_Rider
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+            {
+                Toast.MakeText(this, "Permission was granted", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "Permission was denied", ToastLength.Short).Show();
+            }
         }
 
         public void OnMapReady(GoogleMap googleMap)
         {
             bool success = googleMap.SetMapStyle(MapStyleOptions.LoadRawResourceStyle(this, Resource.Raw.silvermapstyle));
             mainMap = googleMap;
+        }
+
+        bool CheckLocationPermission()
+        {
+            bool permissionGranted = false;
+
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Android.Content.PM.Permission.Granted &&
+                ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) != Android.Content.PM.Permission.Granted)
+            {
+                permissionGranted = false;
+                RequestPermissions(permissionGroupLocation, requestLocationId);
+            }
+            else
+            {
+                permissionGranted = true;
+            }
+
+            return permissionGranted;
+        }
+
+        async void GetMyLocation()
+        {
+            if (!CheckLocationPermission())
+            {
+                return;
+            }
+
+            mLastLocation = await locationClient.GetLastLocationAsync();
+            if (mLastLocation != null)
+            {
+                LatLng myposition = new LatLng(mLastLocation.Latitude, mLastLocation.Longitude);
+                mainMap.MoveCamera(CameraUpdateFactory.NewLatLngZoom(myposition, 17));
+            }
+        }
+
+        void CreateLocationRequest()
+        {
+            mLocationRequest = new LocationRequest();
+            mLocationRequest.SetInterval(UPDATE_INTERVAL);
+            mLocationRequest.SetFastestInterval(FASTEST_INTERVAL);
+            mLocationRequest.SetPriority(LocationRequest.PriorityHighAccuracy);
+            mLocationRequest.SetSmallestDisplacement(DISPLACEMENT);
+            locationClient = LocationServices.GetFusedLocationProviderClient(this);
+            mLocationCallback = new LocationCallbackHelper();
+            mLocationCallback.MyLocation += MLocationCallback_MyLocation;
+        }
+
+        private void MLocationCallback_MyLocation(object sender, LocationCallbackHelper.OnLocationCapturedEventArgs e)
+        {
+            mLastLocation = e.Location;
+            LatLng myposition = new LatLng(mLastLocation.Latitude, mLastLocation.Longitude);
+            mainMap.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(myposition, 12));
+        }
+
+        void StartLocationUpdates()
+        {
+            if (CheckLocationPermission())
+            {
+                locationClient.RequestLocationUpdates(mLocationRequest, mLocationCallback, null);
+            }
         }
     }
 }
